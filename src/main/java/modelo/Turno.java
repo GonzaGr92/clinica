1b91646a22afad5fb67bd011cadb9a7c07b583a8
Cintia <cintudelrio@gmail.com>
2017-12-08 18:50:11 -0300

    Add turno

---
+++ src/main/java/modelo/Turno.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Cintia
+ */
+public class Turno {
+    
+    private Paciente paciente;
+    private Profesional profesional;
+    private LocalDateTime horario;
+
+    public Turno(Paciente paciente, Profesional profesional, LocalDateTime horario) {
+        this.paciente = paciente;
+        this.profesional = profesional;
+        this.horario = horario;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Profesional getProfesional() {
+        return profesional;
+    }
+
+    public void setProfesional(Profesional profesional) {
+        this.profesional = profesional;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+    
+    
+    
+}
