04f095b2d033d70a83fa6dd68c9efbdf0c1938c2
Cintia <cintudelrio@gmail.com>
2017-12-08 18:56:27 -0300

    Add Regimen Horario
    Add Regimen Horario

---
+++ src/main/java/modelo/RegimenHorario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Cintia
+ */
+public class RegimenHorario {
+    
+    private LocalTime entrada;
+    private LocalTime salida;
+    private Dias dia;
+    
+    
+    
+}
